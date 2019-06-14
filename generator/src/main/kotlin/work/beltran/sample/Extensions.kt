2cb9b74710502ee9b4034354377d6445d000daa4
Eric Tsang <surplus.et@gmail.com>
2019-06-13 20:40:34 -0700

    trying to add my own annotation processor

---
+++ generator/src/main/kotlin/work/beltran/sample/Extensions.kt
@@ -0,0 +1,15 @@
+package work.beltran.sample
+
+import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.Element
+import javax.lang.model.element.PackageElement
+
+fun Element.packageName(processingEnv:ProcessingEnvironment):PackageElement
+{
+    return processingEnv.elementUtils.getPackageOf(this)!!
+}
+
+fun Element.qualifiedName(processingEnv:ProcessingEnvironment):String
+{
+    return "${packageName(processingEnv)}.$simpleName"
+}
