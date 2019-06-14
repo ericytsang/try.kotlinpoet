5abf011d114b89693fc4f420e03eb3da6ab30146
Eric Tsang <surplus.et@gmail.com>
2019-06-13 23:14:02 -0700

    moving elements around...

--- generator/src/main/kotlin/work/beltran/sample/Extensions.kt
+++ generator/src/main/kotlin/work/beltran/sample/Extensions.kt
@@ -3,6 +3,7 @@ package work.beltran.sample
 import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.element.Element
 import javax.lang.model.element.PackageElement
+import javax.lang.model.type.MirroredTypesException
 import javax.lang.model.type.TypeMirror
 
 fun Element.packageName(processingEnv:ProcessingEnvironment):PackageElement
@@ -13,4 +14,17 @@ fun Element.packageName(processingEnv:ProcessingEnvironment):PackageElement
 fun Element.qualifiedName(processingEnv:ProcessingEnvironment):String
 {
     return "${packageName(processingEnv)}.$simpleName"
+}
+
+val Visitable.typeMirrors:List<TypeMirror> get()
+{
+    try
+    {
+        classes
+        throw Exception("you're not supposed to be here!")
+    }
+    catch (e:MirroredTypesException)
+    {
+        return e.typeMirrors
+    }
 }
--- generator/src/main/kotlin/work/beltran/sample/Visitable.kt
+++ generator/src/main/kotlin/work/beltran/sample/Visitable.kt
@@ -1,20 +1,5 @@
 package work.beltran.sample
 
-import javax.lang.model.type.MirroredTypesException
-import javax.lang.model.type.TypeMirror
 import kotlin.reflect.KClass
 
 annotation class Visitable(vararg val classes:KClass<*>)
-
-val Visitable.typeMirrors:List<TypeMirror> get()
-{
-    try
-    {
-        classes
-        throw Exception("you're not supposed to be here!")
-    }
-    catch (e:MirroredTypesException)
-    {
-        return e.typeMirrors
-    }
-}
